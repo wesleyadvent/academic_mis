86c797fbbb16dc464d150042ad2dd87e624fe673
wes <wesleyadventch220505@gmail.com>
2025-02-26 11:40:29 +0700

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    protected $table = 'karyawan';
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -18,10 +19,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'nip',
         'name',
-        'email',
         'password',
     ];
+
+    protected $primaryKey = 'nip';
+    public $incrementing = false;
 
     /**
      * The attributes that should be hidden for serialization.
